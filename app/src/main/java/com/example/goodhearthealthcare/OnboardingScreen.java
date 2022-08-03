a5a063ab48e55790943a643cfd217952154ed9ff
kausar-ansarii <kausaransari2002@gmail.com>
2022-08-03 18:25:58 +0530

    Initial commit

---
+++ app/src/main/java/com/example/goodhearthealthcare/OnboardingScreen.java
@@ -0,0 +1,13 @@
+package com.example.goodhearthealthcare;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OnboardingScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_onboarding_screen);
+    }
+}
