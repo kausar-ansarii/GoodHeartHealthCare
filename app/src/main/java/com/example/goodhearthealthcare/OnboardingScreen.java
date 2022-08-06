d94ae735604cc11ca0d4c9cd193c583320f03fe6
kausar-ansarii <kausaransari2002@gmail.com>
2022-08-06 18:52:31 +0530

    Added Gradient File

--- app/src/main/java/com/example/goodhearthealthcare/OnboardingScreen.java
+++ app/src/main/java/com/example/goodhearthealthcare/OnboardingScreen.java
@@ -1,6 +1,6 @@
 package com.example.goodhearthealthcare;
+import androidx.appcompat.app.AppCompatActivity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class OnboardingScreen extends AppCompatActivity {
