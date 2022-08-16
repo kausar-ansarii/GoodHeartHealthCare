b0cbdc0b7b51f78a44d530a7cec9c4995c503557
kausar-ansarii <kausaransari2002@gmail.com>
2022-08-16 20:01:08 +0530

    OTPpage

--- app/src/main/java/com/example/goodhearthealthcare/LoginActivity.java
+++ app/src/main/java/com/example/goodhearthealthcare/LoginActivity.java
@@ -2,13 +2,30 @@ package com.example.goodhearthealthcare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        TextView signUpEditText = (TextView) findViewById(R.id.signupText);
+        signUpEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent otpScreen = new Intent(LoginActivity.this, phoneNumber.class);
+                startActivity(otpScreen);
+
+            }
+        });
+
     }
 }
+
