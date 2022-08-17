b267cf2d71f090748822ff1e42af4246e3cd8787
kausar-ansarii <kausaransari2002@gmail.com>
2022-08-17 19:15:07 +0530

    otp_page

--- app/src/main/java/com/example/goodhearthealthcare/phoneNumber.java
+++ app/src/main/java/com/example/goodhearthealthcare/phoneNumber.java
@@ -2,13 +2,28 @@ package com.example.goodhearthealthcare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class phoneNumber extends AppCompatActivity {
+public class phoneNumber extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phone_number);
+
+        Button otp_btn = (Button) findViewById(R.id.OTP_button);
+
+        otp_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(phoneNumber.this,OTPScreen.class);
+                startActivity(intent);
+            }
+        });
     }
 }
