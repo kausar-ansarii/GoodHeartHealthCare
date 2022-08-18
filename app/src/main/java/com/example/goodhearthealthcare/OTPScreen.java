b93bd858fd80a3934e67e7ce3406758670f280c4
kausar-ansarii <kausaransari2002@gmail.com>
2022-08-18 19:33:35 +0530

    signUpForm

--- app/src/main/java/com/example/goodhearthealthcare/OTPScreen.java
+++ app/src/main/java/com/example/goodhearthealthcare/OTPScreen.java
@@ -2,12 +2,25 @@ package com.example.goodhearthealthcare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class OTPScreen extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_otpscreen);
+        Button verifybutton = (Button)  findViewById(R.id.OTP_button);
+        verifybutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent verify = new Intent(OTPScreen.this,SignupForm.class);
+                startActivity(verify);
+
+            }
+        });
     }
 }
